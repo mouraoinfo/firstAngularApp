dccdf480c2a907f10c5d05b76f1fe568346ba784
mouraoinfo <dev@mourao.info>
2024-08-27 08:20:47 -0300

    feat: Update home component with lazy loading for housing location images

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {RouterModule} from '@angular/router';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+import routes from './app/app.routes';
+import { HousingLocationComponent } from './app/housing-location/housing-location.component';
+import { HousingService } from './app/housing.service';
+
+
+
+
+
+  bootstrapApplication(AppComponent, {
+    providers: [ provideRouter(routes)],
+  }).catch((err) => console.error(err));
