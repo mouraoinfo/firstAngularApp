b14df4854961a2174b73f68d69fbec54b32de757
mouraoinfo <dev@mourao.info>
2024-08-27 04:28:39 -0300

    feat: Add housing location to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,18 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
