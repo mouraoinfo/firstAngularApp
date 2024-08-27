66455df538e1bd5bf0d81b8010ca2b5c9584ff32
mouraoinfo <dev@mourao.info>
2024-08-27 05:18:42 -0300

    chore: Update home and housing-location components with common imports

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import { HousingLocation } from "../housinglocation";
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
